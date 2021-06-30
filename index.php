f3e2a6a3c485e25873e4b303d115c4482e1d061c
Myrax <80508011+Anth-Cha@users.noreply.github.com>
2021-06-30 13:11:24 +0200

    Test upload 2

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <div class="col-md-10">
 
                 <!-- Barre des icônes au dessus de la barre de progression -->
-                <!-- TEST ULPOAD -->
+                <!-- TEST ULPOAD 2 -->
                 <div id="barre_icon">
                     <div id="icon_nouveau_site">
                         <div class="progress_card card mt-5 ms-5 container-fluid">
