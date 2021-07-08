6483d094789e81ff9c8ae4b281d74c1db353b664
unknown <abdelkarim.ben.amar.85@gmail.com>
2021-07-08 17:41:18 +0200

    button suivant

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 
+    // FUNCTION QUI PERMET L'AFFICHAGE DES ICONES BAR APRES AVOIR CLIQUER SUR NEXT
+
     var current_fs, next_fs, previous_fs; //fieldsets
     var opacity;
     var current = 1;
     var steps = $("fieldset").length; //=4
     
-
+    
     setProgressBar(current);
 
     $(".next").click(function () {
@@ -34,12 +36,56 @@ $(document).ready(function () {
             },
             duration: 500
         });
-        $("#nav-list li:nth-child(" + current + ") i").removeClass("fa-circle").addClass("fa-check-circle");
+        var n=current - 1;
+        console.log(n);
+        $("#icon-bar div:nth-child(" + current + ")").css({
+            'display': 'block'});
         setProgressBar(++current);
         $(".steps").text("Étape " + current + " sur " + steps);
         //replace fa-circle by fa-check
 
     });
+
+        //  FUNCTION QUI PERMET UN RETOUR EN ARRIERE DES ETAPES EN CLIQUANT SUR LES ICONES BAR
+
+    $("#icon-bar .icon-item").click(function () {
+        var n = $(this).index() + 1;
+        
+            current_fs = $("fieldset:nth-child(" + current + ")");
+            step_fs = $("fieldset:nth-child(" + n + ")");
+
+            //show the previous fieldset
+            step_fs.show();
+
+            //hide the current fieldset with style
+            current_fs.animate({
+                opacity: 0
+            }, {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    step_fs.css({
+                        'opacity': opacity
+                    });
+                },
+                duration: 500
+            });
+            current = n;
+            //setProgressBar(n);
+            $(".steps").text("Étape " + n + " sur " + steps);
+            if (n == 1) $(".previous").css({
+                'display': 'none'
+            });
+        
+    });
+
+
+
+    // FUNCTION QUI PERMET DE RETOURNER EN ARRIERE AVEC LE BOUTTON RETOUR (<)
 
     $(".previous").click(function () {
         current_fs = $("fieldset:nth-child(" + current + ")")
@@ -72,78 +118,6 @@ $(document).ready(function () {
         });
     });
 
-    // $("#nav-list li").click(function () {
-    //     var n = $(this).index() + 1;
-    //     if (n < current) {
-    //         current_fs = $("fieldset:nth-child(" + current + ")");
-    //         step_fs = $("fieldset:nth-child(" + n + ")");
-
-    //         //show the previous fieldset
-    //         step_fs.show();
-
-    //         //hide the current fieldset with style
-    //         current_fs.animate({
-    //             opacity: 0
-    //         }, {
-    //             step: function (now) {
-    //                 // for making fielset appear animation
-    //                 opacity = 1 - now;
-    //                 current_fs.css({
-    //                     'display': 'none',
-    //                     'position': 'relative'
-    //                 });
-    //                 step_fs.css({
-    //                     'opacity': opacity
-    //                 });
-    //             },
-    //             duration: 500
-    //         });
-    //         current = n;
-    //         //setProgressBar(n);
-    //         $(".steps").text("Étape " + n + " sur " + steps);
-    //         if (n == 1) $(".previous").css({
-    //             'display': 'none'
-    //         });
-    //     }
-    // });
-
-    // ICON BAR 
-
-    // $("").click(function () {
-    //     var m = $(this).index() + 1;
-    //     if (m < current) {
-    //         current_fs = $("fieldset:nth-child(" + current + ")");
-    //         step_fs = $("fieldset:nth-child(" + m + ")");
-
-    //         //show the previous fieldset
-    //         step_fs.show();
-
-    //         //hide the current fieldset with style
-    //         current_fs.animate({
-    //             opacity: 0
-    //         }, {
-    //             step: function (now) {
-    //                 // for making fielset appear animation
-    //                 opacity = 1 - now;
-    //                 current_fs.css({
-    //                     'display': 'none',
-    //                     'position': 'relative'
-    //                 });
-    //                 step_fs.css({
-    //                     'opacity': opacity
-    //                 });
-    //             },
-    //             duration: 500
-    //         });
-    //         current = m;
-    //         //setProgressBar(n);
-    //         $(".steps").text("Étape " + m + " sur " + steps);
-    //         if (m == 1) $(".previous").css({
-    //             'display': 'none'
-    //         });
-    //     }
-    // });
-
 
     // FUNCTION PROGRESS BAR
 
@@ -165,7 +139,9 @@ $(document).ready(function () {
         $("#new_site_choice").toggle(1000);
         $("#new_site_choice").css("display", "flex", "!important");
         $("#new_site_choice").css("justify-content", "right", "!important");
-        $("#design_site").toggle(1000);       
+        $("#design_site").toggle(1000); 
+        $(".next").toggle(1000);
+
     })
 
     $("#design_site").click(function () {
@@ -173,53 +149,26 @@ $(document).ready(function () {
         $("#design_site_choice").css("display", "flex", "!important");
         $("#design_site_choice").css("justify-content", "right", "!important");
         $("#new_site").toggle(1000);
-    })
-
-    $("#goal_choice").click(function () {
-        $("#list_goal_choice").toggle(1000);
-        $("#list_goal_choice").css("display", "flex", "!important");
-        $("#list_goal_choice").css("justify-content", "right", "!important");
-        $("#list_goal_choice").css("flex-wrap", "wrap", "!important");
-       
-    })
-
-    $("#cible_choice").click(function () {
-        $("#list_cible_choice").toggle(1000);
-        $("#list_cible_choice").css("display", "flex", "!important");
-        $("#list_cible_choice").css("justify-content", "right", "!important");
-        $("#list_cible_choice").css("flex-wrap", "wrap", "!important");
-    })
-
-    $("#help_choice").click(function () {
-        $("#list_help_choice").toggle(1000);
-        $("#list_help_choice").css("display", "flex", "!important");
-        $("#list_help_choice").css("justify-content", "right", "!important");
-        $("#list_help_choice").css("flex-wrap", "wrap", "!important");
-    })
-
-    $("#content_choice").click(function () {
-        $("#list_content_choice").toggle(1000);
-        $("#list_content_choice").css("display", "flex", "!important");
-        $("#list_content_choice").css("justify-content", "right", "!important");
-        $("#list_content_choice").css("flex-wrap", "wrap", "!important");
+        $(".next").toggle(1500);
     })
 
 
-    // CHANGE VALIDATION CIRCLE
-
-    $(".choixx").click(function (e) {
-        console.log(this)
-        $("#" + this.id + " .far").toggleClass("fa-circle");
-        $("#" + this.id + " .far").toggleClass("fa-check-circle");
-        $("#" + this.id + " .card").toggleClass("border-3 border-primary");
+     $(".choice").click(function (e) {
+        console.log(this);
+        $("#list_" + this.id).toggle(1000);
+        $("#list_" + this.id).css("display", "flex", "!important");
+        $("#list_" + this.id).css("justify-content", "right", "!important");
+        $("#list_" + this.id).css("flex-wrap", "wrap", "!important");
     })
 
 
     // ALERT WHEN CLICK ON THE CROSS 
 
     $(".close").click(function() {
-        
+
+        $(".css_background_alert").show();
         $(".alert").show();
+        
         
         
     });
@@ -227,12 +176,14 @@ $(document).ready(function () {
     $(".btn-close").click(function() {
 
         $(".alert").hide();
+        $(".css_background_alert").hide();
 
     });
 
     $(".continue_creating").click(function(){
 
         $(".alert").hide();
+        $(".css_background_alert").hide();
 
     });
 
