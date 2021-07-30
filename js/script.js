353b3eed7a59e49cec1bfee868a1eecf0683b644
Assia <assia-z@live.fr>
2021-07-30 12:41:03 +0200

    ajout des range slider + tooltip text

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var current_fs, next_fs, previous_fs, step_fs; //fieldsets
     var opacity;
     var current = 1;
-    var steps = $("fieldset").length-1; //=4
+    var steps = $("fieldset").length - 1; //=4
 
 
     setProgressBar(current);
@@ -211,8 +211,12 @@ $(document).ready(function() {
         $("#" + id1 + "_choice").css("justify-content", "center", "!important");
         $("#" + id2).toggle(600);
         $(".next").fadeToggle("slow");
-        $("#" + id2 + "_choice").find(':input').each(function(index, element) { //reset input
-            element.checked = false;
+        $("#" + id2 + "_choice").find(':input:checkbox').each(function(index, element) { //reset input
+            console.log(element);
+            if (element.checked != false) {
+                element.checked = false;
+                element.dispatchEvent(new Event('change'));
+            }
         });
         $("#icon-bar #" + id2 + "_icon").removeClass("show");
         $("#icon-bar #" + id1 + "_icon").addClass("show");
@@ -254,6 +258,13 @@ $(document).ready(function() {
         $(".error_choice").hide();
     });
 
+    $("input[name=carte_langues]").change(function() {
+        $(".langue").fadeToggle();
+    });
+    $("input[name=carte_referencement]").change(function() {
+        $(".seo").fadeToggle();
+    });
+
 
     $("#last_field input, #last_field select, #last_field textarea").blur(function(e) {
         validation(e.target);
@@ -278,116 +289,3 @@ $(document).ready(function() {
         return resultat;
     }
 });
-// START - RANGE_SLIDER
-var settings = {
-    visible: 0, 
-    theme: {
-      backgroud: "rgba(0,0,0,.9)",
-    },
-    CSSVarTarget: document.querySelector('.range-slider'),
-    knobs: [
-      "Thumb",
-      {
-        cssVar: ['thumb-size', 'px'],
-        label: 'thumb-size',
-        type: 'range',
-        min: 6, max: 33 //  value: 16,
-      },
-      "Value",
-      {
-        cssVar: ['value-active-color'], // alias for the CSS variable
-        label: 'value active color',
-        type: 'color',
-        value: 'white'
-      },
-      {
-        cssVar: ['value-background'], // alias for the CSS variable
-        label: 'value-background',
-        type: 'color',
-      },
-      {
-        cssVar: ['value-background-hover'], // alias for the CSS variable
-        label: 'value-background-hover',
-        type: 'color',
-      },
-      {
-        cssVar: ['primary-color'], // alias for the CSS variable
-        label: 'primary-color',
-        type: 'color',
-      },
-      {
-        cssVar: ['value-offset-y', 'px'],
-        label: 'value-offset-y',
-        type: 'range', value: 5, min: -10, max: 20
-      },
-      "Track",
-      {
-        cssVar: ['track-height', 'px'],
-        label: 'track-height',
-        type: 'range', value: 8, min: 6, max: 33
-      },
-      {
-        cssVar: ['progress-radius', 'px'],
-        label: 'progress-radius',
-        type: 'range', value: 20, min: 0, max: 33
-      },
-      {
-        cssVar: ['progress-color'], // alias for the CSS variable
-        label: 'progress-color',
-        type: 'color',
-        value: '#EEEEEE'
-      },
-      {
-        cssVar: ['fill-color'], // alias for the CSS variable
-        label: 'fill-color',
-        type: 'color',
-        value: '#0366D6'
-      },
-      "Ticks",
-      {
-        cssVar: ['show-min-max'],
-        label: 'hide min/max',
-        type: 'checkbox',
-        value: 'none'
-      },
-      {
-        cssVar: ['ticks-thickness', 'px'],
-        label: 'ticks-thickness',
-        type: 'range',
-        value: 1, min: 0, max: 10
-      },
-      {
-        cssVar: ['ticks-height', 'px'],
-        label: 'ticks-height',
-        type: 'range',
-        value: 5, min: 0, max: 15
-      },
-      {
-        cssVar: ['ticks-gap', 'px'],
-        label: 'ticks-gap',
-        type: 'range',
-        value: 5, min: 0, max: 15
-      },
-      {
-        cssVar: ['min-max-x-offset', '%'],
-        label: 'min-max-x-offset',
-        type: 'range',
-        value: 10, step: 1, min: 0, max: 100
-      },
-      {
-        cssVar: ['min-max-opacity'],
-        label: 'min-max-opacity',
-        type: 'range', value: .5, step: .1, min: 0, max: 1
-      },
-      {
-        cssVar: ['ticks-color'], // alias for the CSS variable
-        label: 'ticks-color',
-        type: 'color',
-        value: '#AAAAAA'
-      },
-    ]
-  }
-  
-  new Knobs(settings)
-  // END - RANGE_SLIDER
-  
